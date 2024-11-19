48c0844aac9daca9cc4211ab1095dae85e021197
John Cottrell <johnc@businesscraft.com.au>
2024-11-19 13:01:50 +1100

    Fix broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -110,7 +110,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/getting-started',
+              to: '/docs/getting-started/businesscraft-overview',
             },
             {
               label: 'News',
